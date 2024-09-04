bbf1233b48679b15fca75ec6646d0fd6db7891bb
Zeratul4340 <eduardoatroom37@gmail.com>
2024-09-04 11:29:53 -0700

    EVEN MORE animation fiddling
    FUNCTIONAL PROGRESS

--- src/main/java/net/omicron43/reliquarymod/mixin/ModelLoaderMixin.java
+++ src/main/java/net/omicron43/reliquarymod/mixin/ModelLoaderMixin.java
@@ -3,7 +3,6 @@ package net.omicron43.reliquarymod.mixin;
 import net.minecraft.client.color.block.BlockColors;
 import net.minecraft.client.render.model.BlockStatesLoader;
 import net.minecraft.client.render.model.ModelLoader;
-import net.minecraft.client.render.model.UnbakedModel;
 import net.minecraft.client.render.model.json.JsonUnbakedModel;
 import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.util.Identifier;
@@ -23,7 +22,7 @@ public abstract class ModelLoaderMixin {
     @Shadow
     protected abstract void loadItemModel(ModelIdentifier modelId);
 
-    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/model/ModelLoader;loadItemModel(Lnet/minecraft/client/util/ModelIdentifier;)V", ordinal = 1, shift = At.Shift.AFTER))
+    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/model/ModelLoader;loadItemModel(Lnet/minecraft/client/util/ModelIdentifier;)V", ordinal = 0, shift = At.Shift.AFTER))
     public void addDeleterCube2D(BlockColors blockColors, Profiler profiler, Map<Identifier, JsonUnbakedModel> jsonUnbakedModels, Map<Identifier, List<BlockStatesLoader.SourceTrackedData>> blockStates, CallbackInfo ci) {
         this.loadItemModel(new ModelIdentifier(Omicron43Reliquary.id("deleter_cube_2d"), "inventory"));
     }
